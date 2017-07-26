fb89f847d59f07b73b64f1c7f713b3a684c4ddb0
Clodoaldo <clodoaldo@webi.com.br>
2017-07-26 11:10:25 -0300

    att

--- application/views/codemin/header_view.php
+++ application/views/codemin/header_view.php
@@ -2,10 +2,11 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
-    <title>Gercont 2 - Painel Administrativo</title>
+    <title><?php echo $configuracoes->titulo;?> - Painel Administrativo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="shortcut icon" href="./public/images/favicon.ico" />
     <base href="<?=base_url()?>">
 
     <!-- CSS -->
